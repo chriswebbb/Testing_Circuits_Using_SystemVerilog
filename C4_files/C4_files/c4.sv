dd7b2540efc39f58502d4cb1f640b2c27db761e2
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 23:25:39 +0100

    Update c4.sv

--- C4_files/C4_files/c4.sv
+++ C4_files/C4_files/c4.sv
@@ -1,26 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: c4
-//            :
-// File name  : c4.sv
-//            :
-// Description: Top level code for C4 Lab exercise
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 28/08/17
-//            : Version 2.0 04/11/20 
-//
-/////////////////////////////////////////////////////////////////////
-
-
 module c4 (output logic s, t, input logic n_clk, rst, a);
 
 logic s_plus, t_plus, s_bar, t_bar, clk;
