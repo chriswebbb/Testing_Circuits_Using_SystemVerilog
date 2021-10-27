5634bc60a8d25102b060a7f2658171106b1f5360
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 23:26:22 +0100

    Update scan_dff.sv

--- C4_files/C4_files/scan_dff.sv
+++ C4_files/C4_files/scan_dff.sv
@@ -1,25 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: scan_dff
-//            :
-// File name  : scan_dff.sv
-//            :
-// Description: Scan D Flip-flop code for C4 Lab exercise
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 28/08/17
-//            : Version 2.0 04/11/20
-//
-/////////////////////////////////////////////////////////////////////
-
 module scan_dff (output logic q, qbar, input logic clk, rst, d, mode, scan_in);
 
 always_ff @(posedge clk, negedge rst)
