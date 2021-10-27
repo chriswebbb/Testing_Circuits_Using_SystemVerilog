ab7db5ba14ddd4b492337fc3c5ff289cc0ad93f8
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 23:26:01 +0100

    Update next_state.sv

--- C4_files/C4_files/next_state.sv
+++ C4_files/C4_files/next_state.sv
@@ -1,28 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: next_state
-//            :
-// File name  : next_state.sv
-//            :
-// Description: Next state logic code for C4 Lab exercise
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 28/08/17
-//            : Version 2.0 04/11/20 Use `define and `ifdef
-//
-/////////////////////////////////////////////////////////////////////
-
-
-// To insert a fault, change ONE of the following lines to `define`
-
 `undef FSA0
 `define FSA1
 
